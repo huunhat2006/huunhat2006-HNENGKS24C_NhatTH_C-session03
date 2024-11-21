cd0e99d27a6041853e2a08ea6768619bfc8b3bb8
Huunhat123 <huunhat20052006@gmail.com>
2024-11-21 20:48:05 +0700

    nộp bài  session03-lession01

---
+++ session03-01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	// khai bao bien
+	char name[100];
+	// nhap gia tri cho bien name tu ban phim
+	printf("Moi nhap ten cua ban:");
+	scanf("%s",&name);
+	//in ten ra man hinh
+	printf("Xin chao %s\n",name);
+	 
+	 return 0; 
+
+ }
+
